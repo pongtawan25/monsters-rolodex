84d3068bb04ef330e1cf6fe50dd985b4f4cbea4c
Pongtawan Kaenprom <pongtawan2541@gmail.com>
2021-07-24 21:20:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(users => {
+        console.log(users)
+        return this.setState({ monsters: users })
+      })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+        </header>
+        {
+          this.state.monsters.map(monster =>
+            <h1 key={monster.id}>{monster.name}</h1>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
